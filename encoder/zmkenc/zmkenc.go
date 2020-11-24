97ec5fb32c4c0641271cd93fefecacfa73ce1e53
Detlef Stern <detlef.stern@hs-heilbronn.de>
2020-11-24 19:42:40 +0100

    Re-License software under the EUPL-1.2-or-later

--- encoder/zmkenc/zmkenc.go
+++ encoder/zmkenc/zmkenc.go
@@ -3,18 +3,9 @@
 //
 // This file is part of zettelstore.
 //
-// Zettelstore is free software: you can redistribute it and/or modify it under
-// the terms of the GNU Affero General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-//
-// Zettelstore is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
-// for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with Zettelstore. If not, see <http://www.gnu.org/licenses/>.
+// Zettelstore is licensed under the latest version of the EUPL (European Union
+// Public License). Please see file LICENSE.txt for your rights and obligations
+// under this license.
 //-----------------------------------------------------------------------------
 
 // Package zmkenc encodes the abstract syntax tree back into Zettelmarkup.
