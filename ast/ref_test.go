f89848585c66436f8efab9346461b5b653bbf9cb
Detlef Stern <detlef.stern@hs-heilbronn.de>
2020-12-18 17:01:36 +0100

    Refactor ast.Reference

--- ast/ref_test.go
+++ ast/ref_test.go
@@ -31,7 +31,8 @@ func TestParseReference(t *testing.T) {
 	for i, tc := range testcases {
 		got := ast.ParseReference(tc.link)
 		if got.IsValid() == tc.err {
-			t.Errorf("TC=%d, expected parse error of %q: %v, but got %q", i, tc.link, tc.err, got)
+			t.Errorf(
+				"TC=%d, expected parse error of %q: %v, but got %q", i, tc.link, tc.err, got)
 		}
 		if got.IsValid() && got.String() != tc.exp {
 			t.Errorf("TC=%d, Reference of %q is %q, but got %q", i, tc.link, tc.exp, got)
@@ -44,26 +45,48 @@ func TestReferenceIsZettelMaterial(t *testing.T) {
 		link       string
 		isZettel   bool
 		isExternal bool
+		isLocal    bool
 	}{
-		{"", false, false},
-		{"http://zettelstore.de/z/ast", false, true},
-		{"12345678901234", true, false},
-		{"12345678901234#local", true, false},
-		{"http://12345678901234", false, true},
-		{"http://zettelstore.de/z/12345678901234", false, true},
-		{"http://zettelstore.de/12345678901234", false, true},
-		{"/12345678901234", false, false},
+		{"", false, false, false},
+		{"http://zettelstore.de/z/ast", false, true, false},
+		{"12345678901234", true, false, false},
+		{"12345678901234#local", true, false, false},
+		{"http://12345678901234", false, true, false},
+		{"http://zettelstore.de/z/12345678901234", false, true, false},
+		{"http://zettelstore.de/12345678901234", false, true, false},
+		{"/12345678901234", false, false, true},
+		{"./12345678901234", false, false, true},
+		{"../12345678901234", false, false, true},
+		{".../12345678901234", false, true, false},
 	}
 
 	for i, tc := range testcases {
 		ref := ast.ParseReference(tc.link)
 		isZettel := ref.IsZettel()
 		if isZettel != tc.isZettel {
-			t.Errorf("TC=%d, Reference %q isZettel=%v expected, but got %v", i, tc.link, tc.isZettel, isZettel)
+			t.Errorf(
+				"TC=%d, Reference %q isZettel=%v expected, but got %v",
+				i,
+				tc.link,
+				tc.isZettel,
+				isZettel)
+		}
+		isLocal := ref.IsLocal()
+		if isLocal != tc.isLocal {
+			t.Errorf(
+				"TC=%d, Reference %q isLocal=%v expected, but got %v",
+				i,
+				tc.link,
+				tc.isLocal, isLocal)
 		}
 		isExternal := ref.IsExternal()
 		if isExternal != tc.isExternal {
-			t.Errorf("TC=%d, Reference %q isExternal=%v expected, but got %v", i, tc.link, tc.isExternal, isExternal)
+			t.Errorf(
+				"TC=%d, Reference %q isExternal=%v expected, but got %v",
+				i,
+				tc.link,
+				tc.isExternal,
+				isExternal)
 		}
 	}
 }
